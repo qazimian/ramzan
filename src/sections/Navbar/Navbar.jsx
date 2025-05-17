c8b062e5c2a438afafdc845203e604ea19529f9d
qazimian <qazimian101@gmail.com>
2025-05-17 16:44:43 +0500

    responseive navbar with side bar is updated

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./NavbarStyle.module.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeNavbar = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <nav className={style.container} id="navbar">
-      <div className={style.logo}><a href="#hero">M. Ramzan</a></div>
-      <div>
-        <ul className={style.navLinks}>
+    <nav className={style.container}>
+      <div className={style.logo}>
+        <a href="#hero" onClick={closeNavbar}>M. Ramzan</a>
+      </div>
+      <ul className={style.navLinks}>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      {/* hamburger ICon */}
+      <div className={style.hamburger} onClick={toggleNavbar}>
+        <span className={style.bar}></span>
+        <span className={style.bar}></span>
+        <span className={style.bar}></span>
+      </div>
+
+      {/* sidebar menu */}
+      <div className={`${style.sidebar} ${isOpen ? style.show : ""}`}>
+        <ul>
           <li>
-            <a href="#about">About </a>
+            <a href="#about">About</a>
           </li>
           <li>
-            <a href="#projects"> Projects </a>
+            <a href="#projects">Project</a>
           </li>
           <li>
             <a href="#skills">Skills</a>
           </li>
           <li>
-            <a href="#contact"> Contact </a>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </div>
+      {isOpen && <div className={style.overlay} onClick={closeNavbar}></div>}
     </nav>
   );
 };
